ff3389968d05a2588bd9a1e59d117c765f5534b5
Akash Kumar <kashkmr27@gmail.com>
2025-08-29 19:13:21 +0100

    google tag manager code

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -85,7 +85,7 @@ export default function RootLayout({ children }) {
                             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','GTM-XXXXXXX');
+                            })(window,document,'script','dataLayer','GTM-M4D224Z');
                         `,
                     }}
                 />
@@ -246,7 +246,7 @@ export default function RootLayout({ children }) {
                 {/* Google Tag Manager (noscript) */}
                 <noscript>
                     <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-M4D224Z"
                         height="0"
                         width="0"
                         style={{ display: 'none', visibility: 'hidden' }}
